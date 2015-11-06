51201acf864a7969e3e01e1988d047286d6edc45
joshanstudent <josephhandy@Josephs-MacBook-Pro.local>
2015-11-05 21:57:53 -0500

    loop, here the user's answer will loop if it is wrong three times and post the correct answer

--- MadLib/main.py
+++ MadLib/main.py
@@ -17,3 +17,24 @@ number = random.randint(1,5)
 
 # Print out results
 print first + ", I am thinging of a number between 1 and 5 "
+
+# loop, here the user's answer will loop if it is wrong three times and post the correct answer
+while guesses < 3:
+    guess = int(raw_input("Take a guess. "))
+    guesses = guesses + 1
+
+    if guess < number:
+        print "Your guess is too low!"
+    if guess > number:
+        print "Your guess is too high!"
+    if guess == number:
+        break
+
+if guess == number:
+    guesses = str(guesses)
+    print "Good job, " + first + " You guessed my number in " + guesses + " guesses"
+if guess != number:
+    number = str(number)
+    print "Nope. The number I was thinking about was actually " + number
+#this code print '\n', create line spacing
+print'\n'
